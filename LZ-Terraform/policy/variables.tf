56d70f5f9e03ef0a9447eb06fcc26ce4a5436fb9
WiseOldTurtle <67073808+WiseOldTurtle@users.noreply.github.com>
2024-09-16 17:17:42 +0100

    updated files
    updated tfvars and added variable references in both variable files and main

--- LZ-Terraform/policy/variables.tf
+++ LZ-Terraform/policy/variables.tf
@@ -1,3 +1,19 @@
+variable "tenant_id" {
+  description = "The Azure Tenant ID."
+  type        = string
+}
+
+variable "client_id" {
+  description = "The Azure Client ID."
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Azure Client Secret."
+  type        = string
+}
+
 variable "subscription_id" {
-  type = string
+  description = "The Azure Subscription ID."
+  type        = string
 }
--- LZ-Terraform/security/variables.tf
+++ LZ-Terraform/security/variables.tf
@@ -9,3 +9,23 @@ variable "location" {
   type        = string
   default     = "UK South"
 }
+
+variable "tenant_id" {
+  description = "The Azure Tenant ID."
+  type        = string
+}
+
+variable "client_id" {
+  description = "The Azure Client ID."
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Azure Client Secret."
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The Azure Subscription ID."
+  type        = string
+}

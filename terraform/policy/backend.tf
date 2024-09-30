88c8c1b6795ad7d2f3dd38dac5f44f86e1942615
WiseOldTurtle <67073808+WiseOldTurtle@users.noreply.github.com>
2024-09-30 21:51:21 +0100

    changedtoENVVariables

--- terraform/policy/backend.tf
+++ terraform/policy/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = var.storage_account_name
-    container_name       = var.container_name
-    key                  = var.state_file
-    resource_group_name  = var.resource_group_name
+    storage_account_name = getenv("TF_VAR_STORAGE_ACCOUNT_NAME")
+    container_name       = getenv("TF_VAR_CONTAINER_NAME")
+    key                  = getenv("TF_VAR_STATE_FILE")
+    resource_group_name  = getenv("TF_VAR_RESOURCE_GROUP_NAME")
   }
 }

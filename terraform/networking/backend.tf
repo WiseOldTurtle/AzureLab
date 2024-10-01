f3fa6c3bf40af45d804318b795a8f73e65b4da92
WiseOldTurtle <67073808+WiseOldTurtle@users.noreply.github.com>
2024-10-01 12:37:28 +0100

    updatedfiles

--- terraform/networking/backend.tf
+++ terraform/networking/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "azurerm" {
-    # Use environment variables for backend configuration
-    storage_account_name = var.storage_account_name
-    container_name       = var.container_name
-    key                  = var.state_file
-    resource_group_name  = var.resource_group_name
-  }
+  backend "azurerm" {}
 }
